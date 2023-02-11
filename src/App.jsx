0e87c07d3dfae4595e888911dddccdb3cd5e3bd4
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-02-11 14:56:04 -0500

    class exercise made

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const URL = 'https://randomuser.me/api?results=3';
+
+  const [users, setUsers] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setUsers(data.results);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
   return (
-    <div>App</div>
+    <div>
+      {
+        users.map(user =>
+          <div key={user.login.uuid}>
+            <img src={user.picture.large} alt={user.name.first} />
+            <p><span>{user.name.first}</span> <span>{user.name.last}</span></p>
+          </div>
+        )
+      }
+    </div>
+
   )
 }
 
