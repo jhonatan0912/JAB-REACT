54c0b9ff64fe360610a724da30d49b23f6522927
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-02-10 21:18:43 -0500

    add events onclick at the image and name

--- src/components/KingCard.jsx
+++ src/components/KingCard.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
+import incognito from "./../assets/rey_incognito.png";
 
 function KingCard({ image, name }) {
+
+  const kingCard = useRef()
+
+  const changeImage = (e) => {
+
+    // kingCard.current.style.backgroundColor = "transparent"
+    e.target.src.includes('incognito') ? e.target.style.visibility = "hidden" : e.target.src = incognito
+    e.target.parentNode.style.backgroundColor = "transparent"
+  }
+
+  const changeText = (e) => {
+    e.target.innerHTML == "Visto" ? e.target.innerHTML = "" : e.target.innerHTML = "Visto"
+  }
   return (
-    <div key={name} className="bg-amber-500 rounded-xl w-10/12 py-10">
-      <img className='m-auto' src={image} alt={name} />
-      <p className='capitalize text-center text-2xl'>{name}</p>
+    <div ref={kingCard} key={name} className="bg-amber-500 rounded-xl w-10/12 py-10">
+      <img className='m-auto' src={image} alt={name} onClick={changeImage} />
+      <p className='capitalize text-center text-2xl' onClick={changeText}>{name}</p>
     </div>
   )
 }
