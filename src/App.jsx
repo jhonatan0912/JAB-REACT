bb124070a43cd841f1abe69c3f69587b9a672e02
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-02-10 22:02:52 -0500

    lesson03 exercice 01 made

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './api/data'
 
 function App() {
+
+  const [reyes, setReyes] = useState(data)
+  const [count, setCount] = useState(0)
+
   return (
-    <div>App</div>
+    <>
+      <button
+        className='border bg-blue-600 text-white p-3 font-bold rounded-md'
+        onClick={() => { setCount(count + 1); count + 1 >= reyes.length && setCount(0) }}
+      >Ver siguiente</button>
+
+      <div>
+        <p className='text-3xl'>La afición principal de <span className='text-red-500'>{reyes[count].nombre}</span> es <span className='text-green-700'>{reyes[count].aficion}</span></p>
+      </div>
+    </>
   )
 }
 
---
+++ src/api/data.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    nombre: "Ataúlfo",
+    aficion: "comer toros sin pelar"
+  },
+  {
+    nombre: "Recesvinto",
+    aficion: "leer a Hegel en arameo"
+  },
+  {
+    nombre: "Teodorico",
+    aficion: "la cría del escarabajo en almíbar"
+  },
+
+]
